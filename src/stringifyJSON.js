b7c4efa1e43c32e681ee69098e1b0b2cda091d77
Arthur Ryabtsev <arthurmoore@arthurs-mbp.gateway.sonic.net>
2018-04-09 14:43:06 -0700

    stringify done

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,39 +1,9 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-	// create new object array 
-	// make all key:value   pairs elements of that array
-	// use Array.toString method
-	// var stringObject = [];
-
-	// if(Array.isArray(obj)) {
-	// 	obj.forEach(function(element) {
-	// 		stringObject.push(element);
-	// 	});
-	// 	return '{ ' + stringObject.join(' ') + ' }';
-	// } else {
-	//   for(var key in obj) {
-	// 	stringObject.push('"', key, ";", '"', obj[key], '"');
-	//   }
-	// return '{ ' + stringObject.join(' ') + ' }';
- //    }
- // 	var answer;
- // 	this = Object.create(obj.prototype);
- // 	if(Array.isArray(this)) {
-	// 	this.forEach(function(element) {
-	// 		answer.push(element);
-	// 	});
-	// 	return answer.toString();
-	// } else {
-	//   for(var key in stringObject) {
-	// 	answer.push([key, obj[key]]);
-	//   }
-	// return answer.toString();
- //    }
-
+	
  	/*
  					 FOLLOW JSON FORMAT 
  	for(var key in obj) {
@@ -47,17 +17,6 @@ var stringifyJSON = function(obj) {
 
 	*/
 
-	// if(typeof obj === 'string') {
-	// 	return '"' + object + '"';
-	// } else if(obj === undefined) {
-	// 	return '"undefined"';
-	// } else if(obj === null) {
-	// 	return '"null"';
-	// } else if(obj === NaN) {
-	// 	return '"NaN"';
-	// } else if(typeof obj = 'number') {
-	// 	return '"' + obj + '"';
-	// }
 
 	if(obj === undefined) {
 		return "undefined";
@@ -67,7 +26,11 @@ var stringifyJSON = function(obj) {
 		return '"' + obj + '"'
 	}
 
-	if(typeof obj === 'number' || typeof obj === null || typeof obj === 'boolean') {
+	if (obj === null) {
+		return "null";
+	}
+
+	if(typeof obj === 'number' || typeof obj === 'boolean') {
 		return obj.toString();
 	}
 
@@ -77,12 +40,15 @@ var stringifyJSON = function(obj) {
 		return '[' + stringifiedArray + ']';
 	}
 
-	if(typeof obj === 'object') {
-		//use recursion again
+	else {
 		var stringifyObjectArray = [];
 		for(var key in obj) {
-			
+			if(typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') {
+
+				stringifyObjectArray.push(stringifyJSON(key),':',stringifyJSON(obj[key]),',');
+			}
 		}
+		return '{' + stringifyObjectArray.slice(0, -1).join('') + '}'
 	}
 
 
@@ -93,7 +59,4 @@ var stringifyJSON = function(obj) {
 
 // }
 
-// var rer = [1, 2, 3, 'hello']
-// console.log(rer.join(' ')
-
-console.log(stringifyJSON([1,[1,2],'string', undefined]));
+console.log(stringifyJSON([1,[1,2],'string', {a:1, b:2}, null, function(){}]),);
